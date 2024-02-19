b892491ca5970c7439d2d7582468dcc7dbaa06e3
Abudau <alexbudau97@gmail.com>
2024-02-19 13:36:06 +0100

    cambios

---
+++ POO/src/boletin8_2_1/RelojDigital.java
@@ -0,0 +1,16 @@
+package boletin8_2_1;
+
+public class RelojDigital extends Reloj {
+	
+	public RelojDigital() {
+		super();
+		
+	}
+
+	@Override
+	public void mostrarHora() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
