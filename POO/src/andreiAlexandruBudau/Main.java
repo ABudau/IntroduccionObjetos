7e218e76513852d3c80df481ba2b5862b837bda8
Abudau <alexbudau97@gmail.com>
2024-03-04 21:50:19 +0100

    cambios

--- POO/src/andreiAlexandruBudau/Main.java
+++ POO/src/andreiAlexandruBudau/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Socio s1 = new Socio("Ana", 10);
-		SocioFundacional socioF = new SocioFundacional("Pedro", 2, LocalDate.now());
+		SocioFundacional socioF = new SocioFundacional("Pedro", 2, LocalDate.of(1990, 05, 8));
 		Socio[]sociosInvitados=new Socio[socioF.getSociosInvitados().length];
 		sociosInvitados[0]=s1;
 		socioF.setSociosInvitados(sociosInvitados);
@@ -28,5 +28,26 @@ public class Main {
 	
 	Club.mostrarReservasRealizadas();
 	
+	}
+	
+	public SocioFundacional socioFundancionalMasAntiguo() {
+		SocioFundacional socioMasAntiguo=null;
+		LocalDate fechaMasAntiguo=LocalDate.now();
+		
+		Socio[] socios=Club.getSociosClub();
+		
+		for (int i = 0; i < socios.length; i++) {
+			Socio socio=socios[i];
+			if (socio!=null) {
+				if (socio instanceof SocioFundacional) {
+					SocioFundacional socioAux =(SocioFundacional)socios[i];
+					if (socioAux.getFechaInscripcion().isBefore(fechaMasAntiguo)) {
+						socioMasAntiguo=socioAux;
+						fechaMasAntiguo=socioAux.getFechaInscripcion();
+					}
+				}
+			}
+		}
+		return socioMasAntiguo;
 	}
 }
